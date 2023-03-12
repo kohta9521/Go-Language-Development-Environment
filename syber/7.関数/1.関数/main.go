44000aa93bffbc8b5de30c7104657d393fff95ec
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 12:40:16 +0900

    add 関数の基礎

--- syber/7.関数/1.関数/main.go
+++ syber/7.関数/1.関数/main.go
@@ -4,11 +4,35 @@ import "fmt"
 
 // 関数
 
-func Plus(x int, y int) int {
+func Plus(x, y int) int {
 	return x + y
+}
+
+func Double(price int) (result int) {
+	result = price * 2
+	return
+}
+
+func Div(x, y int) (int, int) {
+	q := x / y
+	r := x % y
+	return q, r
+}
+
+func Noreturn() {
+	fmt.Println("No return ")
+	return
 }
 
 func main() {
 	i := Plus(1, 2)
 	fmt.Println(i)
+
+	i2, _ := Div(9, 4)
+	fmt.Println(i2)
+
+	i4 := Double(1000)
+	fmt.Println(i4)
+
+	Noreturn()
 }
