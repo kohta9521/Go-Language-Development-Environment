e2a942f1f1eb120ce979cb734f6b687734528fbd
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 12:19:47 +0900

    add 比較演算子

---
+++ syber/6.演算子/2.比較演算子/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// 比較演算子
+
+func main() {
+	fmt.Println(1 == 1)
+	fmt.Println(1 == 2)
+
+	fmt.Println(4 <= 8)
+	fmt.Println(1 >= 9)
+
+	fmt.Println(true == false)
+	fmt.Println(true != false)
+}
