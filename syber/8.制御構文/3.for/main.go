653f6929ac2bc7177da7e063e24f8ee1800e3745
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 13:55:05 +0900

    add for さまざまな定義　活用方法

--- syber/8.制御構文/3.for/main.go
+++ syber/8.制御構文/3.for/main.go
@@ -20,10 +20,33 @@ func main() {
 	// 	point++
 	// }
 
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			continue
-		}
-		fmt.Println(i)
+	// for i := 0; i < 10; i++ {
+	// 	if i == 3 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// arr := [3]int{1, 2, 3}
+	// for i := 0; i < len(arr); i++ {
+	// 	fmt.Println(arr[i])
+	// }
+
+	// for _, v := range arr {
+	// 	fmt.Println(v)
+	// }
+
+	// スライス　配列　可変長
+
+	// sl := []string{"Python", "PHP", "Go"}
+	// for i, v := range sl {
+	// 	fmt.Println(i, v)
+	// }
+
+	m := map[string]int{"Apple": 100, "Banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
 	}
+
 }
