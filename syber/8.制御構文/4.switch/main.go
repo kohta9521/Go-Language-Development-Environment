c011b4a3c178e22ac01d888055ecbb0c7e07a551
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 17:45:00 +0900

    add all files

--- syber/8.制御構文/4.switch/main.go
+++ syber/8.制御構文/4.switch/main.go
@@ -23,4 +23,21 @@ func main() {
 	default:
 		fmt.Println("i don 't knoe")
 	}
+
+	// n := 6
+	// switch {
+	// case n > 0 && n < 4:
+	// 	fmt.Println("0 < n < 4")
+	// case n > 3 && n < 7:
+	// 	fmt.Println("3 < n < 7")
+	// }
+
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("i don 't knoe")
+	}
 }
