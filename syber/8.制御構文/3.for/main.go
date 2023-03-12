122c08ac146665dcea2493f3cc1120b4da5a1978
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 13:47:04 +0900

    add loop for old

--- syber/8.制御構文/3.for/main.go
+++ syber/8.制御構文/3.for/main.go
@@ -5,12 +5,22 @@ import "fmt"
 // 繰り返し構文
 
 func main() {
-	i := 0
-	for {
-		i++
-		if i == 3 {
-			break
-		}
-		fmt.Println("Looop")
+	// i := 0
+	// for {
+	// 	i++
+	// 	if i == 3 {
+	// 		break
+	// 	}
+	// 	fmt.Println("Looop")
+	// }
+
+	// point := 0
+	// for point < 10 {
+	// 	fmt.Println(point)
+	// 	point++
+	// }
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 	}
 }
