b36acc2fa588a81b0f70e2f66f316af3a60b1486
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-12 13:17:11 +0900

    add kuro-zya-

---
+++ syber/7.関数/8.ジェネレーター2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// ジェネレーターの実装
+
+func integers() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	ints := integers()
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+
+	otherints := integers()
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+}
