11931c9ac95e62f0a523dc7f7d3ffffc6a15b435
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-14 15:35:20 +0900

    add label for

--- syber/8.制御構文/6.ラベル付きfor/main.go
+++ syber/8.制御構文/6.ラベル付きfor/main.go
@@ -5,15 +5,28 @@ import "fmt"
 // ラベル付き for
 
 func main() {
-	for {
-		for {
-			for {
-				fmt.Println("START")
-				break
+	// Loop:
+	//
+	//	for {
+	//		for {
+	//			for {
+	//				fmt.Println("START")
+	//				break Loop
+	//			}
+	//			fmt.Println("処理をしない")
+	//		}
+	//		fmt.Println("処理をしない")
+	//	}
+	//	fmt.Println("END")
+
+Loop2:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop2
 			}
-			fmt.Println("処理をしない")
+			fmt.Println(i, j, i*j)
 		}
 		fmt.Println("処理をしない")
 	}
-	fmt.Println("END")
 }
