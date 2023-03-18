67a3b1e0519d2fc97f984e75186a3c1d05df0cb2
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-18 11:06:25 +0900

    add go lang var

--- syber/tohoho/var/main.go
+++ syber/tohoho/var/main.go
@@ -1,13 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-var a1 int = 123
-var a2 = 123
-a3 := 123
-var (
-	ai int = 123
-	a2 := 456
-)
-a1 = 456
-name, age = "Yamada", 37
+	var a1 int = 123
+	a2 := 123
+	fmt.Println(a1, a2)
 }
