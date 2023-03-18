8ba81c17bc5aefdeedeabad33ce2631905817173
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-18 11:49:14 +0900

    add attay and slice lesson

---
+++ syber/tohoho/slice/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a1 := []string{}
+	a1 = append(a1, "red")
+	a1 = append(a1, "Green")
+	a1 = append(a1, "blue")
+	fmt.Println(a1[0], a1[1], a1[2])
+
+	a := []int{}
+	for i := 0; i < 10; i++ {
+		a = append(a, i)
+		fmt.Println(len(a), cap(a))
+	}
+}
