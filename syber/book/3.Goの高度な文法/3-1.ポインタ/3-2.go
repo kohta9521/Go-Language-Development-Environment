19fbb71ac149e32e3e5560509ed82bb6e3ca05dc
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-21 20:28:05 +0900

    add ポインタ操作で値の変更

--- syber/book/3.Goの高度な文法/3-1.ポインタ/3-2.go
+++ syber/book/3.Goの高度な文法/3-1.ポインタ/3-2.go
@@ -8,11 +8,13 @@ func main() {
 	m := 10000
 	p2 := &m
 
-	fmt.Printf("p value:%d, address%p\n", *p, p)
-	fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
+	fmt.Printf("p value:%d, address :  %p\n", *p, p)
+	fmt.Printf("p2 value:%d, address  :  %p\n", *p2, p2)
 
 	pb := p
 	p = p2
 	p2 = pb
 
+	fmt.Printf("p value:%d, address  :  %p\n", *p, p)
+	fmt.Printf("p2 value:%d, address  :  %p\n", *p2, p2)
 }
