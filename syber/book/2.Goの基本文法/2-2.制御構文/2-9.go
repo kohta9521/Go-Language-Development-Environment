cba6eaf5d9be14ca4deb4d3ca6db6bb54f2c50c6
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-21 13:53:41 +0900

    add all time

---
+++ syber/book/2.Goの基本文法/2-2.制御構文/2-9.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"hello"
+	"strconv"
+)
+
+func main() {
+	x := hello.Input("type 1 ~ 5")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Println(x + "までの合計は")
+	} else {
+		return
+	}
+
+	t := 0
+	switch n {
+	case 5:
+		t += 5
+		fallthrough
+	case 4:
+		t += 4
+		fallthrough
+	case 3:
+		t += 3
+		fallthrough
+	case 2:
+		t += 2
+		fallthrough
+	case 1:
+		t += 1
+		fallthrough
+	default:
+		fmt.Println("範囲外です")
+		return
+	}
+
+	fmt.Println(t, "です。")
+}
