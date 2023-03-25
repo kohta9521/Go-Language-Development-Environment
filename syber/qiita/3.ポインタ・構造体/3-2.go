6fe00ddbb73ea3f6b1f4a178aaac2aabca929120
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-25 11:02:07 +0900

    add slice first lesson

--- syber/qiita/3.ポインタ・構造体/3-2.go
+++ syber/qiita/3.ポインタ・構造体/3-2.go
@@ -1,11 +1,18 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	firstName string
 	age       int
 }
 
 func main() {
-	var mike Person
-	mike.firstName
+	// var mike Person
+	// mike.firstName = "Mike"
+	// mike.age = 20
+	// fmt.Println(mike.firstName, mike.age)
+
+	bob := Person{"Bob", 30}
+	fmt.Println(bob.firstName, bob, age)
 }
