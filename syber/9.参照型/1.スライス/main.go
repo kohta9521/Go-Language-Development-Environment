36db6223eabe0540b7c892f3aded0eab4e70b80a
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-25 20:50:56 +0900

    add all files

--- syber/9.参照型/1.スライス/main.go
+++ syber/9.参照型/1.スライス/main.go
@@ -1,8 +1,23 @@
 package main
 
+import "fmt"
+
 // スライス
 // 宣言 操作
 
 func main() {
+	var sl []int
+	fmt.Println(sl)
 
+	var sl2 []int = []int{100, 200}
+	fmt.Println(sl2)
+
+	sl3 := []string{"A", "B"}
+	fmt.Println(sl3)
+
+	sl4 := make([]int, 5)
+	fmt.Println(sl4)
+
+	sl2[0] = 1000
+	fmt.Println(sl2)
 }
