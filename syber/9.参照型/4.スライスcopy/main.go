57d2e8eb3a892fae2e107347a5b55699efb159e3
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-25 21:11:08 +0900

    add copy sccess

--- syber/9.参照型/4.スライスcopy/main.go
+++ syber/9.参照型/4.スライスcopy/main.go
@@ -9,18 +9,18 @@ func main() {
 	// sl := []int{100, 200}
 	// sl2 := sl
 
-	// // sl2[0] = 1000
-	// // fmt.Println(sl)
+	// sl2[0] = 1000
+	// fmt.Println(sl)
+	// fmt.Println(sl2)
 
-	// // var i int = 10
-	// // i2 := i
-	// // i2  = 100
-	// fmt.Println(1, 12)
+	// var i int = 10
+	// i2 := i
+	// fmt.Println(i, i2)
 
 	sl := []int{1, 2, 3, 4, 5}
 	sl2 := make([]int, 5, 10)
-	fmt.Println(sl2)
 	n := copy(sl2, sl)
 
 	fmt.Println(n, sl2)
+
 }
