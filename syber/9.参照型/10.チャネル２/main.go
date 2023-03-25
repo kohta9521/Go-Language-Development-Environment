2078c9c9e0a758e531132d8d5ac291cc6715244b
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-25 21:58:50 +0900

    add chanell first lesson

--- syber/9.参照型/10.チャネル２/main.go
+++ syber/9.参照型/10.チャネル２/main.go
@@ -1,34 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 // channel
-// チャネルとゴルーチン
-
-func reciever(c chan int) {
-	for {
-		i := <-c
-		fmt.Println(i)
-	}
-}
+// 複数のゴルーチン間でのデータの受け渡しをする為に設計されたデータ構造
+// 宣言 操作
 
 func main() {
-	ch1 := make(chan int)
+	var ch1 chan int
+
+	// 受信専用
+	// var ch2 <- chan int
+
+	// 送信専用
+	// var ch3 chan<- int
+
+	ch1 = make(chan int)
+
 	ch2 := make(chan int)
 
-	// fmt.Println(<-ch1)
+	fmt.Println(cap(ch1))
+	fmt.Println(cap(ch2))
 
-	go reciever(ch1)
-	go reciever(ch2)
+	ch3 := make(chan int, 5)
+	fmt.Println(cap(ch3))
 
-	i := 0
-	for i < 100 {
-		ch1 <- i
-		ch2 <- i
-		time.Sleep(50 * time.Millisecond)
-		i++
-	}
+	ch3 <- 3
+	fmt.Println(len(ch3))
+
+	ch3 <- 2
+	fmt.Println(len(ch3))
+
+	ch3 <- 1
+	fmt.Println(len(ch3))
+
+	i := <-ch3
+	fmt.Println(i)
+
+	i2 := <-ch3
+	fmt.Println(i2)
 }
