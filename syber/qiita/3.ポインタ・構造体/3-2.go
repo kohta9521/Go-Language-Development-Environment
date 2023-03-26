0a2ef4c72d2723098f0a926039ba38653f9a088d
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-26 18:38:13 +0900

    add 3-2 and 3-1

--- syber/qiita/3.ポインタ・構造体/3-2.go
+++ syber/qiita/3.ポインタ・構造体/3-2.go
@@ -1,18 +1,6 @@
 package main
 
-import "fmt"
-
 type Person struct {
-	firstName string
-	age       int
-}
-
-func main() {
-	// var mike Person
-	// mike.firstName = "Mike"
-	// mike.age = 20
-	// fmt.Println(mike.firstName, mike.age)
-
-	bob := Person{"Bob", 30}
-	fmt.Println(bob.firstName, bob, age)
+	fistName string
+	age      int
 }
