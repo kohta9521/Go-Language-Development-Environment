e23b70ac92d9b0e953fcd6e049487922c6093f98
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-26 16:23:03 +0900

    ddddd

--- udemy/7.関数/1.関数/main.go
+++ udemy/7.関数/1.関数/main.go
@@ -8,6 +8,11 @@ func Plus(x, y int) int {
 	return x + y
 }
 
+func Double(price int) (result int) {
+	result = price * 2
+	return
+}
+
 func Div(x, y int) (int, int) {
 	q := x / y
 	r := x % y
@@ -18,6 +23,9 @@ func main() {
 	i := Plus(1, 2)
 	fmt.Println(i)
 
-	i2, i3 := Div(9, 3)
-	fmt.Println(i2, i3)
+	i2, _ := Div(9, 3)
+	fmt.Println(i2)
+
+	i4 := Double(1000)
+	fmt.Println(i4)
 }
