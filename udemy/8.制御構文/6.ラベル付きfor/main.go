04e1549687516d682deb8bb9c13744868121cb29
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-26 23:42:24 +0900

    add all fiels

--- udemy/8.制御構文/6.ラベル付きfor/main.go
+++ udemy/8.制御構文/6.ラベル付きfor/main.go
@@ -1,8 +1,28 @@
 package main
 
+import "fmt"
+
 // ラベル付き
 // 条件分岐
 
 func main() {
+	// Loop:
+	//
+	//	for {
+	//		for {
+	//			for {
+	//				fmt.Println("START")
+	//				break Loop
+	//			}
+	//			fmt.Println("処理をしない")
+	//		}
+	//		fmt.Println("処理をしない")
+	//	}
+	//	fmt.Println("END")
 
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			fmt.Println(i, j, i*j)
+		}
+	}
 }
