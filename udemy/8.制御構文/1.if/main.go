a5f087cda4003b0e34756ec7d25aedf02311d9e3
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-26 18:31:47 +0900

    add new files about if and swithc for

---
+++ udemy/8.制御構文/1.if/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// if
+// 条件分岐
+
+func main() {
+	a := 1
+	if a == 2 {
+		fmt.Println("two")
+	} else if a == 1 {
+		fmt.Println("one")
+	} else {
+		fmt.Println("I dont know")
+	}
+}
