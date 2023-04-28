492c35702c738769f98ef44b077a323e0fe3b924
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-28 11:50:39 +0900

    add slice 可変長引数

--- udemy/9.参照型/6.可変長引数/main.go
+++ udemy/9.参照型/6.可変長引数/main.go
@@ -1,8 +1,25 @@
 package main
 
+import "fmt"
+
 // スライス
 // 可変長引数
 
-func main() {
+func sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
+	}
+	return n
+}
 
+func main() {
+	fmt.Println(sum(1, 2, 3))
+	fmt.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+	fmt.Println(sum())
+
+	// slice
+	sl := []int{1, 2, 3}
+	fmt.Println(sum(sl...))
 }
