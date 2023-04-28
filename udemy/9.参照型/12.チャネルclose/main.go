e5232b1da800ebbdf26196a7da67f970145278b8
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-28 12:21:50 +0900

    add go runtime error about fo channel closed

---
+++ udemy/9.参照型/12.チャネルclose/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// channel
+// close
+
+func main() {
+	ch1 := make(chan int, 2)
+
+	close(ch1)
+
+	// ch1 <- 1
+
+	// 受信は可能
+	// fmt.Println(<-ch1)
+
+	i, ok := <-ch1
+	fmt.Println(i, ok)
+}
