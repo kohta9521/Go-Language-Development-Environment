a3490b83dc9114da89491830603abb5a379a5b12
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-07 05:45:23 +0900

    add int lesson

--- agent/4.基本型/1.int/main.go
+++ agent/4.基本型/1.int/main.go
@@ -2,6 +2,22 @@ package main
 
 // 型　整数型
 
-func main() {
+import "fmt"
 
+func main() {
+	var i int = 100
+	fmt.Println(i)
+
+	var i2 int64 = 200
+	fmt.Println(i2)
+
+	fmt.Println(i + 50)
+
+	// fmt.Println(i + i2)
+
+	// 型変換
+	fmt.Printf("%T\n", i2)
+	fmt.Printf("%T\n", int32(i2))
+
+	fmt.Println(i + int(i2))
 }
