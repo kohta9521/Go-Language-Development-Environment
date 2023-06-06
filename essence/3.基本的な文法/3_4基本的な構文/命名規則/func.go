dc661dcf7fe6943214a0590290245fed6260ea9f
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-06 16:54:15 +0900

    add fucntion useris checker

---
+++ essence/3.基本的な文法/3_4基本的な構文/命名規則/func.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func FindUser(name string) (*User, error) {
+	user, err := findUserFromList(users, name)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func main() {
+	user, err := FindUser("Bob")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(user.Name)
+}
