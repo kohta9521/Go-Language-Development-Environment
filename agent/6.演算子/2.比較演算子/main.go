796c96a41a932a67261480de2ac576eaef28711c
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-07 15:17:52 +0900

    add 比較演算子

--- agent/6.演算子/2.比較演算子/main.go
+++ agent/6.演算子/2.比較演算子/main.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 // 比較演算子
 
 func main() {
+	fmt.Println(1 == 1)
 
+	fmt.Println(1 == 2)
+
+	fmt.Println(4 <= 8)
+
+	fmt.Println(8 <= 1)
+
+	fmt.Println(1 < 8)
+
+	// 論理値をそのまま比較
+	fmt.Println(true == false)
+	fmt.Println(true != false)
 }
