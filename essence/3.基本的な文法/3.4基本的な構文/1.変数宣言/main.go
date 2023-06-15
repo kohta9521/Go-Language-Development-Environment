0e1ccc1dc720655e298d957d1c353937e167b2db
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-15 10:55:26 +0900

    add 2023/06/15 first commit about var

---
+++ essence/3.基本的な文法/3.4基本的な構文/1.変数宣言/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 変数宣言
+
+func main() {
+	var n int = 1
+
+	x := 1
+	y := 1.2
+
+	n = 1 + (x+2)*int(y)
+
+	fmt.Println(n)
+
+	fmt.Println(n)
+
+	s := "Hello" + "World"
+	fmt.Println(s)
+}
