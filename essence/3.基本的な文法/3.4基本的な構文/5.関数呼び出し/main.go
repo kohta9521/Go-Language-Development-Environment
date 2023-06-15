a1c47eff34af5e1beb16b55382f1b2f22f231d76
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-15 11:21:16 +0900

    add label for and channel for

--- essence/3.基本的な文法/3.4基本的な構文/5.関数呼び出し/main.go
+++ essence/3.基本的な文法/3.4基本的な構文/5.関数呼び出し/main.go
@@ -1,22 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+// func FindUser(name string) (*User, error) {
+// 	user, err := findUserFromList(users, name)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return user, err
+// }
 
-func FindUser(name string) (*User, error) {
-	user, err := findUserFromList(users, name)
-	if err != nil {
-		return nil, err
-	}
-	return user, err
-}
-
-func main() {
-	user, err := FindUser("Bob")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(user.name)
-}
+// func main() {
+// 	// user, err := FindUser("Bob")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(user.name)
+// }
