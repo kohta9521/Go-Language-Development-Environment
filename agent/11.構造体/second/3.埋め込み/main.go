59e8643b3de2c628ef7534dc31ed935ebfeeb719
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-19 00:04:49 +0900

    add all firls

---
+++ agent/11.構造体/second/3.埋め込み/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// 構造体
+// 埋め込み
+
+type T struct {
+	User User
+}
+
+type User struct {
+	Name string
+	Age  int
+	// X, Y int
+}
+
+func main() {
+	t := T{User: User{Name: "user1", Age: 10}}
+	fmt.Println(t)
+	fmt.Println(t.User)
+	fmt.Println(t.User.Name)
+}
