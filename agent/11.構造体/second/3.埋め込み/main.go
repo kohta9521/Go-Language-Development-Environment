7abadb1a46eb8360e932eb89b5222fc933062947
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-19 08:27:48 +0900

    add 2023/06/19 first commit

--- agent/11.構造体/second/3.埋め込み/main.go
+++ agent/11.構造体/second/3.埋め込み/main.go
@@ -2,22 +2,24 @@ package main
 
 import "fmt"
 
-// 構造体
-// 埋め込み
-
 type T struct {
-	User User
+	User
 }
-
 type User struct {
 	Name string
 	Age  int
-	// X, Y int
 }
+
+// func (u *User) SetName() {
+// 	u.Nama = "A"
+// }
 
 func main() {
 	t := T{User: User{Name: "user1", Age: 10}}
 	fmt.Println(t)
+
 	fmt.Println(t.User)
 	fmt.Println(t.User.Name)
+	fmt.Println(t.User.Age)
+
 }
