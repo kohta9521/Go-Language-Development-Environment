eb6d6a48f5385616d60dcfddd2f280de6350a215
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-18 23:47:20 +0900

    add user

---
+++ agent/11.構造体/second/2.メソッド/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// struct methods
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func (u User) SayName() {
+	fmt.Println(u.Name)
+}
+
+// 参照渡し
+func (u User) SetName(name string) {
+	u.Name = name
+}
+
+func (u *User) SetName2(name string) {
+	u.Name = name
+}
+
+func main() {
+	user1 := User{Name: "user1"}
+	user1.SayName()
+
+	user1.SetName("A")
+	user1.SayName()
+
+	user1.SetName2("A")
+	user1.SayName()
+}
