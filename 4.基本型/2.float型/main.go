8bdd93a089831f99228e3e5652ddc690c92475e8
k_95.21 <kohta0421kochi@gmail.com>
2024-08-06 03:17:32 +0900

    add new float lesson

---
+++ 4.基本型/2.float型/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 型
+// 浮動小数展型
+
+func main() {
+	var fl64 float64 = 2.4
+	fmt.Println(fl64)
+
+	fl := 3.2
+	fmt.Println(fl)
+
+	fmt.Printf("%T\n", fl64)
+
+	var fl32 float32 = 3.2
+	fmt.Print(fl32)
+}
